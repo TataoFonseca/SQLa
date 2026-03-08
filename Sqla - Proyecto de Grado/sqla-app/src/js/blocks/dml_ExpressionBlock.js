5c1d1f8867456f567ff6a47a2424cc0e6dfba542
Tatao Fonseca <exptecnologia.09@gmail.com>
2026-03-08 13:15:02 -0500

    Update dml_ExpressionBlock.js

--- Sqla - Proyecto de Grado/sqla-app/src/js/blocks/dml_ExpressionBlock.js
+++ Sqla - Proyecto de Grado/sqla-app/src/js/blocks/dml_ExpressionBlock.js
@@ -1,30 +1,6 @@
 // // sqla-app/src/js/blocks/expressionBlock.js
 // // SOLO EXPORTA: La forma (JSON) y el generador (función)
 
-// export const EXPRESSION_DEFINITION = {
-//   "type": "sql_expression",
-//   "message0": "'' %1 ''",
-//   "args0": [
-//     {
-//       "type": "field_input",
-//       "name": "EXPRESSION",
-//       "text": ""
-//     }
-//   ],
-//   "output": null,
-//   "colour": 230,
-//   "tooltip": "Expresión SQL libre (columnas, funciones, operaciones, alias, etc.)",
-//   "helpUrl": ""
-// };
-
-// export const EXPRESSION_GENERATOR = function(block, generator) {
-//   const expression = block.getFieldValue('EXPRESSION');
-//   return [expression, generator.ORDER_NONE];
-// };
-
-///////
-
-// sqla-app/src/js/blocks/dml_ExpressionBlock.js
 // MEJORADO: Con soporte para menú contextual de DISTINCT/TOP
 
 export const EXPRESSION_DEFINITION = {
