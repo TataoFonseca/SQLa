09f18a9a2c918a1131bf5811accd01173bea4ae3
Tatao Fonseca <exptecnologia.09@gmail.com>
2026-04-02 21:24:25 -0500

    Adding some fixes to JOIN

--- Sqla - Proyecto de Grado/sqla-backend/src/services/sqlTransformer.service.js
+++ Sqla - Proyecto de Grado/sqla-backend/src/services/sqlTransformer.service.js
@@ -308,7 +308,7 @@ function deduplicateSelectColumns(ast) {
  */
 export function transformSQL(sql, schemaName) {
     try {
-        const opt = { database: 'Postgresql' };
+        const opt = { database: 'TransactSQL' };
 
         console.log(' Transformando SQL:', sql);
         console.log(' Con schema:', schemaName);
